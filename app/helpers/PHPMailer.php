c95970d0a460301276c890575fe3479a3e636405
devosema <benaliosama3@gmail.com>
2024-08-03 15:56:56 +0100

    fix bugs

--- app/helpers/PHPMailer.php
+++ app/helpers/PHPMailer.php
@@ -3,8 +3,10 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
+use Dotenv\Dotenv;
 
-// require '../../vendor/autoload.php';
+$dotenv = Dotenv::createImmutable( __DIR__ . '/../../');
+$dotenv->load();
 
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
@@ -18,8 +20,8 @@ $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TL
 
 $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
 $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-$mail->Username   = 'bms93951@gmail.com';                     //SMTP username
-$mail->Password   = 'usbpazmfmciavtmp';                               //SMTP password
+$mail->Username   = $_ENV["PHPHMAILER_USERNAME"];                     //SMTP username
+$mail->Password   = $_ENV["PHPHMAILER_PASSWORD"];                               //SMTP password
 
 $mail->isHTML(true);
 return $mail;
